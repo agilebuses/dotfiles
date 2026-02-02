41f4a4e766234d8e8be89048fa0a91ca6a2f96c3
Alan Carr <alan@agilebuses.org>
2026-02-02 21:11:58 +0000

    (#1) Added nvim config

---
+++ .config/nvim/lua/keybinds.lua
@@ -0,0 +1,1 @@
+vim.keymap.set('', '<C-s>', ':w<CR>')
---
+++ .config/nvim/lua/options.lua
@@ -0,0 +1,7 @@
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.incsearch = true
+vim.o.hlsearch = true
+vim.o.laststatus = 2
+vim.o.showmode = false
+
